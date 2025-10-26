c092dbf6b321ba7b2b9b02aa9ab85cab638ca656
saidam90 <saida.murtazaliyeva@yahoo.com>
2025-10-26 17:50:12 +0400

    Fix the bug with transportation

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,25 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Index from "./pages/Index";
-// import Circuits from "./pages/Circuits";
-import Gear from "./pages/Gear";
-import Accommodation from "./pages/Accommodation";
-import Transportation from "./pages/Transportation";
 import NotFound from "./pages/NotFound";
-import FAQ from "./pages/FAQ";
-
-const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            {/* <Route path="/circuits" element={<Circuits />} /> */}
-            <Route path="/gear" element={<Gear />} />
-            <Route path="/accommodation" element={<Accommodation />} />
-            <Route path="/transportation" element={<Transportation />} />
-            <Route path="/faq" element={<FAQ />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </ThemeProvider>
-  </QueryClientProvider>
+  <ThemeProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </ThemeProvider>
 );
 
 export default App;

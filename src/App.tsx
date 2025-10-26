4f3fb4218987f2ee3b17dd60a21aca2e69fd00cc
saidam90 <saida.murtazaliyeva@yahoo.com>
2025-10-26 17:52:48 +0400

    Fix build: remove unused Sonner toaster dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -10,7 +9,6 @@ const App = () => (
   <ThemeProvider>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
